9a3801f73f65629f3b311dc14c489658ec5ddefd
ed <edward.alexander.cant@gmail.com>
2020-07-05 08:22:36 +0100

    add temporary win and lost fight buttons. plus a death page

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,12 +16,12 @@ export default {
   data() {
     return {
       pages: [],
+      current_page: undefined,
       button_links: undefined,
-      player: {name: "Hero", health: 100, items: [], image: "//player.jpeg"},
+      player: { name: "Hero", health: 100, items: [], image: "//player.jpeg" },
       player_location: "intro",
       current_monster: undefined
     };
-
   },
   components: {
     narrative: Narrative,
@@ -34,38 +34,60 @@ export default {
       console.log(`turning to page ${page_button.button_destination}`);
       this.turnToPage(page_button);
     });
+
+    eventBus.$on("fight-won", () => {
+      // TODO: add note about the fight being won
+      console.dir(this.current_page);
+      console.dir(this.current_page.destination_after_fight);
+      this.turnToPage({
+        button_destination: this.current_page.destination_after_fight,
+        button_text: "won the fight"
+      });
+    });
+
+    eventBus.$on("fight-lost", () => {
+      this.turnToPage({
+        button_destination: "died",
+        button_text: "lost the battle.."
+      });
+    });
   },
   methods: {
     turnToPage(page_button) {
       const page_name = page_button.button_destination; //this gets the destination we're interested in from the button. We will pull the chapter matching this name from the db.
-      JSONService.getPage(page_name).then(page => { //this pulls the page from the db that matched the destination name. 
-        if (page == null) { //error_handling
-          console.log( //error_handling
+      JSONService.getPage(page_name).then(page => {
+        //this pulls the page from the db that matched the destination name.
+        if (page == null) {
+          //error_handling
+          console.log(
+            //error_handling
             `Error: Null page returned from database for ${page_name}.` //error_handling
           ); //error_handling
-        } else { 
+        } else {
+          this.current_monster = undefined;
+          this.current_page = page;
           console.dir(page); //like console.log but we can look inside the object
           this.player_location = page_name;
           const array_length = this.pages.length;
           if (array_length > 0) {
             this.pages[array_length - 1].narrative_text += "\n\n";
-            this.pages[array_length - 1].narrative_text +=
-              "You decide to ";
+            this.pages[array_length - 1].narrative_text += "You decide to ";
             this.pages[array_length - 1].narrative_text +=
               page_button.button_text;
           }
           this.pages.push(page);
           this.button_links = page.buttons;
           if ("fight_monster" in page) {
-            this.startFight(page)
+            this.startFight(page);
           }
         }
-      })
+      });
     },
     startFight(page) {
-      const monster_name = page.fight_monster
-      JSONService.getMonster(monster_name)
-      .then(dbMonster => this.current_monster = dbMonster)
+      const monster_name = page.fight_monster;
+      JSONService.getMonster(monster_name).then(
+        dbMonster => (this.current_monster = dbMonster)
+      );
     }
   }
 };
@@ -84,7 +106,7 @@ export default {
   align-items: center;
   display: flex;
   background-image: url("./assets/map_background.jpg");
-  background-size: cover; 
+  background-size: cover;
   background-position: center;
 }
 </style>
--- client/src/components/Fight.vue
+++ client/src/components/Fight.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="fight">
     <div class="health">Health:{{player.health}}</div>
-    <div class="monster" v-if="monster">
-      <h2>{{ monster.name }}</h2>
-      <img src="https://placebear.com/g/100/100" /> <br />
-      <i>"{{ monster.name }}"</i>
-      <div class="health">Health:{{ monster.health }}</div>
+    <div v-if="monster">
+      <div class="monster" v-if="monster">
+        <h2>{{ monster.name }}</h2>
+        <img src="https://placebear.com/g/100/100" />
+        <br />
+        <i>"{{ monster.name }}"</i>
+        <div class="health">Health:{{ monster.health }}</div>
+      </div>
+
+      <input type="submit" class="button" v-on:click="winClicked" value="win fight" />
+      <input type="submit" class="button" v-on:click="dieClicked" value="die" />
     </div>
   </div>
 </template>
 
 <script>
+import { eventBus } from "../main.js";
+
 export default {
   props: ["player", "monster"],
   methods: {
     numGenerator() {
       return Math.ceil(Math.random() * 20);
+    },
+
+    winClicked() {
+      eventBus.$emit("fight-won", {});
+    },
+    dieClicked() {
+      eventBus.$emit("fight-lost", {});
     }
   }
 };
