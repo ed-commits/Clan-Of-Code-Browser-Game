633a1a959f2f63aaea5b637249a1b4ec0e2f37dc
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-07-07 11:43:45 +0100

    map changes

--- client/src/components/StoryMap.vue
+++ client/src/components/StoryMap.vue
@@ -13,51 +13,53 @@ export default {
     props: ["pages"],
     data(){
         return {
-
-            currentPageCoords: [2000,700]
+            currentPageCoords: [4000,3000],
+            map: undefined,
+            rc: undefined,
 
         }
     },
+
     mounted(){
         eventBus.$on("page-turned", (page) => {
             this.currentPageCoords = page.map_coords
+            this.map.flyTo(this.rc.unproject(this.currentPageCoords), 5, {
+            animate: true,
+            duration: 5
+            });
         }),
         this.leafletInit()
-    },
-    
- 
- methods: {
-        leafletInit(){
-            var L = require('leaflet')
-            L.RasterCoords = require('leaflet-rastercoords')
-            var img = [
-                7932,  // original width of image (here from `example/karta.jpg`)
-                6228   // original height of image
-            ]
-            var pageCoords = this.currentPageCoords
-            console.log(pageCoords)
-            // create the map
-            var map = L.map('map')
-            // assign map and image dimensions
-            var rc = new L.RasterCoords(map, img)
-            map.setMaxZoom(rc.zoomLevel())
-            // all coordinates need to be unprojected using the `unproject` method
-            // set the view in the lower right edge of the image
-            map.setView(rc.unproject(pageCoords),9)
-            map.on('click', function (event) {
-                 // any position in leaflet needs to be projected to obtain the image coordinates
-                var coords = rc.project(event.latlng) 
-                var zoom_level = map.getZoom()
-                console.log(zoom_level)
-                var marker = L.marker(rc.unproject(coords))
-                    .addTo(map)
-                marker.bindPopup('[' + Math.floor(coords.x) + ',' + Math.floor(coords.y) + ']')
-                    .openPopup()
-            })
-            L.tileLayer("/assets/tiles/{z}/{x}/{y}.png", {
-            noWrap: true
-            }).addTo(map)            
-
+    },   
+    methods: {
+            leafletInit(){
+                const L = require('leaflet')
+                L.RasterCoords = require('leaflet-rastercoords')
+                const img = [
+                    7932,  // original width of image (here from `example/karta.jpg`)
+                    6228   // original height of image
+                ]
+                const pageCoords = this.currentPageCoords
+                // create the map
+                this.map = L.map('map', { zoomControl: false, attributionControl: false})
+                // assign map and image dimensions
+                this.rc = new L.RasterCoords(this.map, img)
+                this.map.setMaxZoom(this.rc.zoomLevel())
+                // all coordinates need to be unprojected using the `unproject` method
+                // set the view in the lower right edge of the image
+                this.map.setView(this.rc.unproject(pageCoords),9)
+                this.map.on('click', function (event) {
+                    // any position in leaflet needs to be projected to obtain the image coordinates
+                    const coords = this.rc.project(event.latlng) 
+                    const zoom_level = this.map.getZoom()
+                    console.log(zoom_level)
+                    const marker = L.marker(this.rc.unproject(coords))
+                        .addTo(this.map)
+                    marker.bindPopup('[' + Math.floor(coords.x) + ',' + Math.floor(coords.y) + ']')
+                        .openPopup()
+                })
+                L.tileLayer("/assets/tiles/{z}/{x}/{y}.png", {
+                noWrap: true
+                }).addTo(this.map)            
         },
         
 
